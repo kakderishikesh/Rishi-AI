bf7a53d459e0a83c80758480d7e24d2a453635b7
kakderishikesh <rishikesh.kakde59@gmail.com>
2025-04-30 00:00:40 -0400

    Updated Starting Prompts

--- frontend/src/lib/starterPrompts.ts
+++ frontend/src/lib/starterPrompts.ts
@@ -1,7 +1,7 @@
 // starterPrompts.ts
 export const STARTER_PROMPTS = [
-    "Why should we hire you?",
-    "What are your technical skills?",
-    "What projects have you worked on?",
-    "Tell us about your latest job role.",
-  ];
+  "Tell me about your latest achivements.",
+  "What are your technical skills?",
+  "What projects have you worked on?",
+  "Tell us about your latest job role.",
+];
